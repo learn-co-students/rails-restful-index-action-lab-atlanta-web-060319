ab38cf67d454288d29f9590f507a6df2bf66c41c
github username <github email address>
2019-06-30 13:32:35 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+ get '/', to: 'students#index'
+ #get '/', to: 'example#testing'
+
+ resources :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
